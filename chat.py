be55eb40a66d71736febcd08c42f98558ab79a2f
hemang11 <hemangshrimali22@gmail.com>
2020-10-11 19:04:41 +0530

    trained model

--- chat.py
+++ chat.py
@@ -7,16 +7,15 @@ from chatterbot.trainers import ListTrainer
 
 chatbot = ChatBot(
     'Darkrai-Bot',
-    read_only=True,
     preprocessors=[
         'chatterbot.preprocessors.clean_whitespace'
     ],
-    #storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[
         'chatterbot.logic.BestMatch',
         'chatterbot.logic.MathematicalEvaluation',
-    ]
-    #database_uri='sqlite:///database.sqlite3'
+    ],
+    database_uri='sqlite:///database.sqlite3'
 )
 general = [
     "Hi",
@@ -40,16 +39,35 @@ general = [
 ]
 
 project =[
-    'What is Darkrai?',
-    'Darkrai is a browser extension that will enable the user to chat with any other user visiting the same website as themselves.',
-    'Is it an extension',
-    'Yes Darkrai is an extension'
+     'What is Darkrai?',
+     'Darkrai is a browser extension that will enable the user to chat with any other user visiting the same website as themselves.',
+     'How to use',
+     '''
+     Download the Extension from the Chrome and Open any Website and then you can chat with any other person or
+     group of person visitin the website
+     ''',
     'Who built it?',
     'It is built by students of DA-IICT college',
-    'Nice',
-    'Yes',
+    'What features does it have?',
+    'Darkrai is a browser extension through which we chat with other people visiting the same website',
+    'Is it available for FireFox',
+    'No currently it is available for Chrome only',
+    'Is it a full fledged application',
+    'It is still in development and new features are added continuously',
+    'Is it applicable for any website',
+    'Yes it is available for any websites',
+    'Can i use it?',
+    'Yes you can use it you just have to download the extension from the chrome',
+    'Can i get the Github link of this project',
+    r'Yes you can go to this repository https://github.com/darkraichat',
+    'Is it open-source or close source',
+    'Darkrai Chat is an Open Source Application',
+    'Which stack Darkrai uses?',
+    'MERN stack',
     'What is the Stack of Darkrai',
-    'It uses MERN stack'
+    'It uses MERN stack',
+    'This is a Nice App',
+    'Thanks do share with your friends'
 ]
 
 trainer = ListTrainer(chatbot)
@@ -58,12 +76,13 @@ for item in (general, project):
     trainer.train(item)
 
 if __name__ == "__main__":
-
-    user_input = str(sys.argv[1])
-    bot_response = chatbot.get_response(user_input)
-    print(type(bot_response))
-    if(bot_response.confidence < 0.1):
-        print('I am sorry, but I do not understand.')
-    else:
-        print(bot_response)
-    sys.stdout.flush()
+    while True:
+        try:
+            user_input = input('User : ')
+            bot_response = chatbot.get_response(user_input)
+            if(bot_response.confidence < 0.2):
+                print('I am sorry, but I do not understand.')
+            else:
+                print(f'Chat-Bot : {bot_response}')
+        except(KeyboardInterrupt, EOFError, SystemExit):
+            break
